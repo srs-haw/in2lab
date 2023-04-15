1bb757d6af91c60640eb24a6c210a7cf1bce9572
infwhs831 <whs831@haw-hamburg.de>
2023-04-15 19:53:26 +0200

    New project from IntelliJ to GitLab

--- src/test/java/com/haw/srs/customerservice/MovieRepositoryTest.java
+++ src/test/java/com/haw/srs/customerservice/MovieRepositoryTest.java
@@ -1,11 +1,10 @@
 package com.haw.srs.customerservice;
 
-import com.haw.srs.customerservice.Application;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -13,9 +12,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles(profiles = "testing")
 public class MovieRepositoryTest {
 
     @Autowired
--- src/test/java/com/haw/srs/customerservice/ReservationRepositoryTest.java
+++ src/test/java/com/haw/srs/customerservice/ReservationRepositoryTest.java
@@ -4,17 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.persistence.metamodel.IdentifiableType;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles(profiles = "testing")
 public class ReservationRepositoryTest {
 
     @Autowired
